16a068a95222fc50a8231dcad1acc31511978319
Ariel Maniago <admaniago01@gmail.com>
2023-12-24 23:02:27 +0800

    fix width on mobile

--- src/lib/components/homepage/FeaturedPortfolio.svelte
+++ src/lib/components/homepage/FeaturedPortfolio.svelte
@@ -2,7 +2,7 @@
 	import FeaturedCard from './featured/FeaturedCard.svelte';
 </script>
 
-<div class="w-[65vw] leading-5">
+<div id="featured-card" class="w-[65vw] leading-5">
 	<h2 class="lg:text-4xl md:text-2xl text-2xl mb-2">Latest Portfolio</h2>
 	<p class="mb-4">"The only way to do great work is to love what you do." - Steve Jobs</p>
 
@@ -10,4 +10,9 @@
 </div>
 
 <style>
+	@media only screen and (max-width: 767px) {
+		#featured-card {
+			width: 80vw;
+		}
+	}
 </style>
