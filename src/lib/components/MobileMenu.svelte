46d87922e6720f068d678056b77a4fced83561dc
Ariel Maniago <admaniago01@gmail.com>
2024-01-21 10:09:15 +0800

    adding some minor changes

--- src/lib/components/MobileMenu.svelte
+++ src/lib/components/MobileMenu.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { navlinks } from '../navLinks.js';
+	import me from '$lib/images/me.webp';
 
 	let details;
 
@@ -67,21 +68,12 @@
 		<div class="divider my-0"></div>
 		<li>
 			<a class:active-details={routeId == '/about'} class="flex justify-between" href="/about"
-				>About <span
-					><svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-						/>
-					</svg></span
+				>About <span class="flex"
+					><div class="avatar">
+						<div class="w-7 rounded-full">
+							<img class="grayscale" src={me} alt="me" />
+						</div>
+					</div></span
 				></a
 			>
 		</li>
